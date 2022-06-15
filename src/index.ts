7522002c5a6ffe4e90091a6d823a65b8e1acef4e
Martin Obermeier <martin.obermeier@gmail.com>
2022-06-15 20:50:56 -0300

    Primer commit

---
+++ src/index.ts
@@ -0,0 +1,22 @@
+import express from 'express' // ESModules
+// const express = require('express') // commonjs
+
+import diaryRouter from './routes/diaries'
+
+const app = express()
+
+app.use(express.json()) // middleware que transforma la req.body a un json
+
+const PORT = 3000
+
+// app.get("/ping", (req , res) => {
+app.get('/ping', (_, res) => {
+  console.log('someone pinged here!!')
+  res.send('pong')
+})
+
+app.use('/api/diaries', diaryRouter)
+
+app.listen(PORT, () => {
+  console.log(`Server running on port ${PORT}`)
+})
